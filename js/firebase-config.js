991ec29dc2fcffe5df8f24e01366b2c6990d947b
dalalpavitar <dalalpavitar@gmail.com>
2026-06-27 17:49:20 +0530

    Add Firebase realtime DB

---
+++ js/firebase-config.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyBrXRJyJuqvQ5ikAOwoBthIojJSZ3KwyKY",
+  authDomain: "cdlu-university.firebaseapp.com",
+  projectId: "cdlu-university",
+  storageBucket: "cdlu-university.firebasestorage.app",
+  messagingSenderId: "336912868141",
+  appId: "1:336912868141:web:4c8fa6e630bb44c27229a4",
+  measurementId: "G-J3RTKW92VR"
+};
+
+firebase.initializeApp(firebaseConfig);
+var db = firebase.firestore();
